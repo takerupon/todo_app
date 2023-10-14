252cb254d8ca360939fda684b941688264c8ae0c
takerupon <takerup0927@icloud.com>
2023-10-15 00:40:05 +0900

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,17 @@ import Image from 'next/image'
 import { useColorMode } from '@chakra-ui/react'
 
 export default function Home() {
-  const { toggleColorMode } = useColorMode();
 
   return (
     <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
       <Flex direction={'column'} background="gray.100" padding={12} rounded={6}>
-        <Heading mb={6}> Log In</Heading>
+        <Flex align={"center"} justify={"center"}>
+          <Heading mb={6}> Log In</Heading>
+        </Flex>
         <Input placeholder="sample@sample.com" variant="filled" mb={3} type="email" />
         <Input placeholder="*********" variant="filled" mb={6} type="password" />
         <Button mb={6} colorScheme="teal" type="submit">Log In</Button>
-        <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+        <Button mb={6} colorScheme="teal" type="submit">Sign Up</Button>
       </Flex>
     </Flex>
   )
